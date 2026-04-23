784889b8ddbdf34d4eeef598c3fd920e7d7753fa
rodriknd <rp752989@gmail.com>
2026-04-23 11:03:47 -0400

    feat: Creando el componente principal solo interfaz

---
+++ components/login/ButtonLoginGoogle.tsx
@@ -0,0 +1,32 @@
+import { Pressable, Text } from "react-native";
+import { useAuth } from "@/contexts/AuthContext";
+import { getCurrentUser } from "@/services/userService";
+import { useRouter } from "expo-router";
+import AntDesign from '@expo/vector-icons/AntDesign'
+
+export default function ButtonLoginGoogle() {
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const user = await getCurrentUser();
+      login(user);
+      router.replace('/(tabs)');
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
+  return (
+    <Pressable
+      className="bg-white border border-gray-300 rounded-full px-8 py-2 mt-4 flex flex-row items-center gap-2"
+      onPress={handleLogin}
+    >
+      <AntDesign name="google" size={30} color="blue" />
+      <Text className={"text-black font-semibold text-[16px]"}>
+        Login with Google
+      </Text>
+    </Pressable>
+  );
+}

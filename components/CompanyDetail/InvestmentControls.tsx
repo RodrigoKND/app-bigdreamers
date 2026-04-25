4e31a17372f6f41057a58f05d482b8dcfb537905
rodriknd <rp752989@gmail.com>
2026-04-25 17:34:49 -0400

    feat: Implementando la interfaz compania detalle

--- components/CompanyDetail/InvestmentControls.tsx
+++ components/CompanyDetail/InvestmentControls.tsx
@@ -4,20 +4,22 @@ import Button from '@/components/shared/Button';
 export default function InvestmentControls({ currentGems }: { currentGems: number }) {
     return (
         <View className="mb-8">
-            <Text className="text-xl font-bold mb-2">Sobre la empresa</Text>
-            <Text className="text-gray-500 leading-5 mb-6">
+            <Text className="text-xl font-bold mb-2 dark:text-white text-black">Sobre la empresa</Text>
+            <Text className="dark:text-gray-100 text-gray-600 leading-5 mb-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.
             </Text>
 
-            <View className="flex-row items-center justify-between bg-gray-50 p-4 rounded-3xl border border-gray-100">
+            <View className="flex-row items-center justify-between bg-white/5 p-4 rounded-3xl border dark:border-gold-500 border-blue-primary">
                 <View>
-                    <Text className="text-xs text-gray-400 uppercase font-bold">Inversión estimada</Text>
-                    <Text className="text-lg font-bold text-indigo-600">{currentGems} Gemas</Text>
+                    <Text className="text-xs text-gold-500 uppercase font-bold">Tus Gemas</Text>
+                    <Text className="text-lg font-bold dark:text-white text-black">{currentGems} G</Text>
                 </View>
-
+                
+                {/* TODO: Implementar el método para invertir y deshabilitar el button
+                si las currentGems es menor a las gemas actuales de la empresa */}
                 <Button
                     title="Invertir"
-                    variant='primary'
+                    variant='secondary'                    
                     size='md'
                     className="px-8 rounded-full py-3"
                     onPress={() => { }}

ec956a74d359cd445e5955079deb26de186be4cc
rodriknd <rp752989@gmail.com>
2026-04-25 12:17:55 -0400

    feat: Añadiendo niveles oro, plata y bronce en las empresas

--- components/invest/Invest.tsx
+++ components/invest/Invest.tsx
@@ -1,13 +1,15 @@
 import { Text, View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import CompanyCard from '@/components/invest/CompanyCard';
 import OfferItem from '@/components/invest/OfferItem';
 import SearchBar from '@/components/shared/SearchBar';
+import LevelGold from '@/components/invest/levelGold/LevelGold';
+import LevelSilver from '@/components/invest/levelSilver/LevelSilver';
+import LevelBronce from '@/components/invest/levelBronce/LevelBronce';
 
 export default function Invest() {
   return (
     <SafeAreaView className="flex-1 bg-blue-primary" edges={['top']}>
-      
+
       <View className="p-6">
         <Text className="dark:text-white text-black text-3xl font-bold">
           Empresas disponibles
@@ -17,37 +19,23 @@ export default function Invest() {
         </Text>
       </View>
 
-      <ScrollView 
+      <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 40 }} // Espacio extra al final para que no quede pegado
+        contentContainerStyle={{ paddingBottom: 30 }}
       >
         <View className="px-6 mt-6">
-          <SearchBar />
-
-          <View className="flex-row justify-between items-center mb-4">
-            <Text className="text-lg font-bold dark:text-white text-black">Destacadas</Text>
-            <Text className="dark:text-gray-300 text-black">Ver todas</Text>
-          </View>
           
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <CompanyCard 
-              name="Tech Innovate" 
-              gems={4500} 
-              imageUrl="https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6XQ2V2HNRRMDHDJ55MPMNBJ4E4.jpg" 
-            />
-            <CompanyCard 
-              name="Green Earth" 
-              gems={2800} 
-              imageUrl="https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6XQ2V2HNRRMDHDJ55MPMNBJ4E4.jpg" 
-            />
-          </ScrollView>
+          <SearchBar />
+          <LevelGold />
+          <LevelSilver />
+          <LevelBronce />
 
           <View className="mt-8 mb-4 flex-row justify-between items-center">
             <Text className="text-lg font-bold dark:text-white text-black">Ofertas</Text>
             <Text className="dark:text-gray-300 text-black text-xs">Ver todas</Text>
           </View>
 
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} className='fixed bottom-0 p-4'>
             <OfferItem name="Empresa X" gems={4000} imageUrl="https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6XQ2V2HNRRMDHDJ55MPMNBJ4E4.jpg" />
             <OfferItem name="Empresa Y" gems={1200} imageUrl="https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6XQ2V2HNRRMDHDJ55MPMNBJ4E4.jpg" />
             <OfferItem name="Empresa Z" gems={850} imageUrl="https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6XQ2V2HNRRMDHDJ55MPMNBJ4E4.jpg" />

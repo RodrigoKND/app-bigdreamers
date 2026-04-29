84bd8153501d945c9bb8ef6cf1980245a17f73a4
rodriknd <rp752989@gmail.com>
2026-04-29 12:18:32 -0400

    fix: Actualizando la ruta de importación de los servicios

--- app/login.tsx
+++ app/login.tsx
@@ -2,7 +2,7 @@ import Login from '@/components/login/Login';
 import { useAuth } from '@/contexts/AuthContext';
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
-import { getCurrentUser } from '@/services/userService';
+import { getCurrentUser } from '@/services/supabase/userService';
 
 export default function LoginScreen() {
   const { login, isLoggedIn } = useAuth();
